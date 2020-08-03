6e32cb1031e9dcbaa31ac4aab460a885b0ad1b7f
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-08-03 22:18:16 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
 class Owner
-  # code goes here
+  # or I can do create a getter method
+  #   def name
+  #     @name
+  #   end
+  @@all=[]
+  attr_reader :name, :species
+  def initialize (name)
+    @name=name
+    @species="human"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def say_species
+  "I am a #{@species}."
+  end
+  def cats
+    Cat.all.select {|cat| cat.owner==self}
+  end
+  def dogs
+    
+  end
 end
